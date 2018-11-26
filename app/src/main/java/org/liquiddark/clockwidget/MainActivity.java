eb4d234d660f5121b1da2c050fa41a353231c15d
mehedee-hassan <razin.meha@gmail.com>
2018-11-26 17:28:50 +0600

    clock

--- app/src/main/java/org/liquiddark/clockwidget/MainActivity.java
+++ app/src/main/java/org/liquiddark/clockwidget/MainActivity.java
@@ -2,12 +2,28 @@ package org.liquiddark.clockwidget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.okbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
